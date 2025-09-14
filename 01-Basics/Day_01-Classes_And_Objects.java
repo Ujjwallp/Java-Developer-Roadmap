745d5ffe6b0ba31d0beff8c341defbb2281c442b
UJJWAL PRAKASH <ujjwallp22@gmail.com>
2025-09-14 23:07:18 +0530

    Update Day_01-Classes_And_Objects.java

--- 01-Basics/Day_01-Classes_And_Objects.java
+++ 01-Basics/Day_01-Classes_And_Objects.java
@@ -3,25 +3,51 @@
 
 public class Day_01_Classes_And_Objects {
 
-    // Q1. Student Class
-    // TODO
+    /*
+     Q1. Create a class Student with variables id and name.
+         Create 2 objects and print their details.
+    */
+    // TODO: Write your solution here
 
-    // Q2. Dog Class
-    // TODO
 
-    // Q3. Car Class
-    // TODO
+    /*
+     Q2. Create a class Dog with variable breed and method bark().
+         Create 2 objects and call the method.
+    */
+    // TODO: Write your solution here
 
-    // Q4. Object Independence
-    // TODO
 
-    // Q5. BankAccount Class
-    // TODO
+    /*
+     Q3. Create a class Car with variables brand, model, and price.
+         Create 3 objects with different values and print them.
+    */
+    // TODO: Write your solution here
 
-    // Q6. Employee Array
-    // TODO
+
+    /*
+     Q4. Demonstrate that changing the variable of one object
+         does not affect another object’s variable.
+    */
+    // TODO: Write your solution here
+
+
+    /*
+     Q5. Create a class BankAccount with instance variables accountNumber and balance.
+         - Create multiple objects and set different balances.
+         - Print balances separately to show independence.
+    */
+    // TODO: Write your solution here
+
+
+    /*
+     Q6. Create a class Employee with variables id, name, and salary.
+         - Create an array of 5 employees.
+         - Print all details using a loop.
+    */
+    // TODO: Write your solution here
+
 
     public static void main(String[] args) {
-        // 🚀 Implement answers for above questions here.
+        // 🚀 Implement answers for above questions here
     }
 }
